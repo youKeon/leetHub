9d76e69ca6be444d4c36ad7ae260c649f44b4d6e
YuKeon <96862049+youKeon@users.noreply.github.com>
2023-05-31 22:01:28 +0900

    Time: 54 ms (40.52%), Space: 44 MB (13.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,16 +5,11 @@ class Solution {
         
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i+1; j <nums.length; j++) {
-                int sum = nums[i] + nums[j];
-                if (sum == target) {
-                    ans.add(i);
-                    ans.add(j);
-                    flag = true;
-                    break;
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
                 }
             }
-            if (flag) break; 
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return null;
     }
 }
