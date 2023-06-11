ffc9449e793c1f730c8b9efb3ab0bf9e5a3b3b86
youKeon <96862049+youKeon@users.noreply.github.com>
2023-06-11 14:27:23 +0900

    Time: 0 ms (100.00%), Space: 39.7 MB (93.71%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,10 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        else if(p==null || q==null) return false;
-        
-        if (p.val == q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        else return false;
+        return check(p, q);
+    }
+    
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return check(p.left, q.left) && check(p.right, q.right);
     }
 }
